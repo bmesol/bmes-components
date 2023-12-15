c2552815bc6580a3049768f17cfff3a256210dab
Gokulkrishnan47 <gokulkrishnan20092002@gmail.com>
2023-12-14 18:19:13 -0800

    "label component added"

---
+++ src/lib/components/Label/Label.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { LabelVariants, LabelProps } from "./DTOs";
+import "./Label.scss";
+// --- Label component ---
+const Label = (props: LabelProps) => {
+  const { type = "", variant, classNames = "", href = "", label = "" } = props;
+
+  return (
+    <label className={`label ${variant} ${type} ${classNames}`}>
+      {type === "link" ? <a href={href}>{label}</a> : <span>{label}</span>}
+    </label>
+  );
+};
+
+export { Label, LabelVariants };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from "./components/Button/Button"; // Button Component
 export * from "./components/TextBox/TextBox"; // TextBox Component
 export * from "./components/Pin/Pin"; // Pin Component
 export * from "./components/Card/Card"; // Card component
+export * from "./components/Label/Label"; //Label Component
