37cc0c2a2e725ee320ae2ecd268d1d342f7bea03
Sengottaiyan-BMES <sengottaiyan@bmesolutions.in>
2024-01-14 11:31:48 +0530

    Toast Docs added

--- src/stories/Toast/Toast.stories.tsx
+++ src/stories/Toast/Toast.stories.tsx
@@ -1,25 +1,59 @@
 import React from "react";
 import { ToastProvider, useToast } from "./../../lib/components/Toast/Toast";
-import ToastDoc from "./ToastDoc.md";
 import { Button, ButtonVariants } from "../../lib/components/Button/Button";
 
 const meta = {
   title: "Components/Toast",
-  tags: ["autodocs"],
   component: ToastProvider,
   parameters: {
     layout: "centered",
-    docs: {
-      description: {
-        component: ToastDoc,
-      },
-    },
   },
 };
 
 export default meta;
 
-export const Toast = {
+export const Toast_Message = {
+  render: (children: React.ReactElement): JSX.Element => {
+    const App = () => {
+      const toast = useToast();
+      return (
+        <>
+          <Button
+            variant={ButtonVariants.PRIMARY}
+            label="Toast Info"
+            classNames="bg-dark text-bg-dark m-3"
+            onClick={() => toast.info("Message text here.")}
+          />
+          <Button
+            variant={ButtonVariants.SUCCESS}
+            label="Toast Success"
+            classNames="m-3"
+            onClick={() => toast.success("Message text here.")}
+          />
+          <Button
+            variant={ButtonVariants.WARNING}
+            label="Toast Warning"
+            classNames="m-3"
+            onClick={() => toast.warning("Message text here.")}
+          />
+          <Button
+            variant={ButtonVariants.DANGER}
+            label="Toast Error"
+            classNames="m-3"
+            onClick={() => toast.error("Message text here.")}
+          />
+        </>
+      );
+    };
+    return (
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    );
+  },
+};
+
+export const Toast_Heading_Message = {
   render: (children: React.ReactElement): JSX.Element => {
     const App = () => {
       const toast = useToast();
