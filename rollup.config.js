35d8fc845f63dbc1f5cfbede84fceec389495f11
Sengottaiyan-BMES <sengottaiyan@bmesolutions.in>
2024-03-13 17:42:54 +0530

    Release 0.0.37-beta

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import { terser } from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import image from '@rollup/plugin-image';
-import svgr from '@svgr/rollup';
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import image from "@rollup/plugin-image";
+import svgr from "@svgr/rollup";
 
 export default {
-  input: 'src/index.ts', // Entry point of your library
+  input: "src/index.ts", // Entry point of your library
   output: [
     {
-      file: 'dist/bundle.js',
-      format: 'cjs', // CommonJS format
+      file: "dist/bundle.js",
+      format: "cjs", // CommonJS format
       sourcemap: true,
     },
     {
-      file: 'dist/bundle.mjs',
-      format: 'es', // ES Module format
+      file: "dist/bundle.mjs",
+      format: "es", // ES Module format
       sourcemap: true,
     },
   ],
@@ -26,10 +26,18 @@ export default {
     resolve(), // Resolve node_modules
     commonjs(), // Convert CommonJS modules to ES6
     typescript(), // Handle TypeScript files
-    postcss(), // Process CSS files
+    postcss({
+      plugins: [
+        require("postcss-import"),
+        require("tailwindcss")("./tailwind.config.js"),
+        require("autoprefixer"),
+      ],
+      extract: false, // Extract CSS to separate file
+      minimize: true, // Minimize CSS
+    }), // Process CSS files
     terser(), // Minify output
     image(), // Handling image import
     svgr(), // import SVG as React components.
   ],
-  external: ['react', 'react-dom'], // Specify external dependencies
+  external: ["react", "react-dom"], // Specify external dependencies
 };
