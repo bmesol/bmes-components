7755179fa7e7b404adcaf0da0db48611d18e790f
Sengottaiyan-BMES <sengottaiyan@bmesolutions.in>
2024-03-19 09:49:49 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,9 @@ export const theme = {
         DEFAULT: "hsl(var(--card))",
         foreground: "hsl(var(--card-foreground))",
       },
+      theme1: {
+        DEFAULT: "var(--theme1-color)",
+      },
     },
     borderRadius: {
       lg: `var(--border-radius)`,
